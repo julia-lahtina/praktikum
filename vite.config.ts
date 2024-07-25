ae4bca2169fe5c82a7945ffd5a6947fb45347d4a
julia-lahtina <j.lakhtin@gmail.com>
2024-07-25 18:52:29 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite'
+
+import { devDependencies, peerDependencies } from './package.json'
+
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      // the proper extensions will be added
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        'react/jsx-runtime',
+        ...Object.keys(devDependencies),
+        ...Object.keys(peerDependencies),
+      ],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
 })
